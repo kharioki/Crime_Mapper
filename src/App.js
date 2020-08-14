6e78874914189f372f148b2e9dc22eab27bd20d7
kharioki <antolisah@gmail.com>
2020-08-14 22:57:33 +0300

    using SWRconfig

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import useSWR, { SWRConfig } from 'swr';
 // create an async function
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
-function App() {
+export default function App() {
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <Crimes />
+    </SWRConfig>
+  );
+}
+
+function Crimes() {
   const url =
     'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10';
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url);
 
   if (error) return <div>Error...</div>;
   if (!data) return <div>Loading...</div>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <DisplayCrimes crimes={data} />;
 }
 
-export default App;
+function DisplayCrimes({ crimes }) {
+  return <pre>{JSON.stringify(crimes, null, 2)}</pre>;
+}
