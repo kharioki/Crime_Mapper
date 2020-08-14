6f54f64031df735d059a77bbad659efc0910ec3b
kharioki <antolisah@gmail.com>
2020-08-15 00:25:29 +0300

    refactor and mapbox config

---
+++ src/AllCrimes.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import useSWR, { SWRConfig } from 'swr';
+
+// create an async function
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+export default function App() {
+  return (
+    // we use revalidateOnFocus: false to prevent a refetch every time the browser tab is focused
+    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
+      <Crimes />
+    </SWRConfig>
+  );
+}
+
+function Crimes() {
+  const url =
+    'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10';
+  const { data, error } = useSWR(url);
+
+  if (error) return <div>Error...</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <DisplayCrimes
+      crimes={data}
+      categories={[...new Set(data.map(crime => crime.category))]}
+    />
+  );
+}
+
+function DisplayCrimes({ crimes, categories }) {
+  const [filterCategory, setFilterCategory] = useState(null);
+
+  const filteredCrimes = filterCategory
+    ? crimes.filter(crime => crime.category === filterCategory)
+    : crimes;
+
+  return (
+    <>
+      {categories.map(category => (
+        <button onClick={() => setFilterCategory(category)} key={category}>
+          {category}
+        </button>
+      ))}
+
+      {filterCategory && (
+        <button onClick={() => setFilterCategory(null)}>reset</button>
+      )}
+
+      <pre>{JSON.stringify(filteredCrimes, null, 2)}</pre>
+    </>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import useSWR, { SWRConfig } from 'swr';
+import ReactMapGL, { Marker, FlyToInterpolator } from 'react-map-gl';
+import useSupercluster from 'use-supercluster';
+import { token } from './config';
 
-// create an async function
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+function CrimeMap() {
+  // setup map
+  const [viewport, setViewport] = useState({
+    latitude: 52.6376,
+    longitude: -1.135171,
+    width: '100vw',
+    height: '100vh',
+    zoom: 12
+  });
+
+  const mapRef = useRef();
+
+  // load and prepare data
+
+  // get map bounds
+
+  // get clusters
+
+  // return map
+  return (
+    <ReactMapGL {...viewport} maxZoom={20} mapboxApiAccessToken={token}>
+      {/*markers */}
+    </ReactMapGL>
+  );
+}
 
 export default function App() {
-  return (
-    // we use revalidateOnFocus: false to prevent a refetch every time the browser tab is focused
-    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
-      <Crimes />
-    </SWRConfig>
-  );
-}
-
-function Crimes() {
-  const url =
-    'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10';
-  const { data, error } = useSWR(url);
-
-  if (error) return <div>Error...</div>;
-  if (!data) return <div>Loading...</div>;
-
-  return (
-    <DisplayCrimes
-      crimes={data}
-      categories={[...new Set(data.map(crime => crime.category))]}
-    />
-  );
-}
-
-function DisplayCrimes({ crimes, categories }) {
-  const [filterCategory, setFilterCategory] = useState(null);
-
-  const filteredCrimes = filterCategory
-    ? crimes.filter(crime => crime.category === filterCategory)
-    : crimes;
-
-  return (
-    <>
-      {categories.map(category => (
-        <button onClick={() => setFilterCategory(category)} key={category}>
-          {category}
-        </button>
-      ))}
-
-      {filterCategory && (
-        <button onClick={() => setFilterCategory(null)}>reset</button>
-      )}
-
-      <pre>{JSON.stringify(filteredCrimes, null, 2)}</pre>
-    </>
-  );
+  return <CrimeMap />;
 }
