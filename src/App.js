bd0c681815d064133b74e392a9d243e63bf9d029
kharioki <antolisah@gmail.com>
2020-08-15 00:45:32 +0300

    fetch crime data and render markers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import useSWR, { SWRConfig } from 'swr';
 import ReactMapGL, { Marker, FlyToInterpolator } from 'react-map-gl';
 import useSupercluster from 'use-supercluster';
 import { token } from './config';
+import './App.css';
+
+// create an async function
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 function CrimeMap() {
   // setup map
@@ -17,6 +21,10 @@ function CrimeMap() {
   const mapRef = useRef();
 
   // load and prepare data
+  const url =
+    'https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10';
+  const { data, error } = useSWR(url, fetcher);
+  const crimes = data && !error ? data.slice(0, 100) : [];
 
   // get map bounds
 
@@ -24,8 +32,26 @@ function CrimeMap() {
 
   // return map
   return (
-    <ReactMapGL {...viewport} maxZoom={20} mapboxApiAccessToken={token}>
+    <ReactMapGL
+      {...viewport}
+      maxZoom={20}
+      mapboxApiAccessToken={token}
+      onViewportChange={newViewport => {
+        setViewport({ ...newViewport });
+      }}
+    >
       {/*markers */}
+      {crimes.map(crime => (
+        <Marker
+          key={crime.id}
+          latitude={parseFloat(crime.location.latitude)}
+          longitude={parseFloat(crime.location.longitude)}
+        >
+          <button className="crime-marker">
+            <img src="/custody.svg" alt="custody" />
+          </button>
+        </Marker>
+      ))}
     </ReactMapGL>
   );
 }
